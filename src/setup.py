2f3a5d30cf447753d5c251bfa40d4441f5d07a10
jldupont <github@jldupont.com>
2011-03-06 18:05:52 -0500

    2.0.0-a1

--- src/setup.py
+++ src/setup.py
@@ -45,6 +45,10 @@ PyUSB offers easy USB devices communication in Python.
 It should work without additional code in any environment with
 Python >= 2.4, ctypes and an pre-built usb backend library
 (currently, libusb 0.1.x, libusb 1.x, and OpenUSB).
+
+This version "g2" is meant to address shortcomings of the
+original 'pyusb' package as to support for the Dlink DNS-323 environment
+running with Python / funplug.
 """
 )
 
